5880b50da690a233fd36478ff45191f9e078f055
Harshitha Shetty <141444342+HarshithaShetty27@users.noreply.github.com>
2024-12-12 12:32:35 +0530

    Navbar Styling

--- pokedex/src/sections/Navbar.tsx
+++ pokedex/src/sections/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import pokeballIcon from "../assets/pokeball-icon.png"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="pokeball-icon" />
+      </div>
+      <div className="data"></div>
+      <div className="block">
+        <GiHamburgerMenu/>
+      </div>
+    </nav>
   )
 }
 
